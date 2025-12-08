bee74f54f8e20c41e0e37002ccfdeaaec9a7337b
hinryd <lkhh@protonmail.com>
2025-12-08 14:37:38 +0800

    add image optimization with sharp, svgo, and vite-plugin-image-optimizer dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,6 +23,13 @@
 	<title>Henry | HARD CHROME V7</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin="anonymous" />
+
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=PT+Mono&family=Syne:wght@700;800&family=Spline+Sans+Mono:ital,wght@0,300..700;1,300..700&&family=Geist:wght@100..900&display=swap"
+	/>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" />
 
 	<link rel="icon" href={favicon} />
 </svelte:head>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		ViteImageOptimizer({
+			webp: {
+				quality: 70
+			},
+			jpeg: {
+				quality: 70
+			}
+		}),
+		sveltekit()
+	]
 });
